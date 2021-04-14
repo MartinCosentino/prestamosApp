678569b157c587502c820cecf3b8407b46fff79c
Martin Cosentino <martincosentino95@gmail.com>
2021-04-14 04:42:09 -0300

    Updates

---
+++ precache-manifest.ed7732754ba3bc240b6c7e58e1764408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54eed4592c5aaebada3504a66abddac1",
+    "url": "/prestamosApp/index.html"
+  },
+  {
+    "revision": "b8c04e14aa7b61962026",
+    "url": "/prestamosApp/static/css/main.22198d4d.chunk.css"
+  },
+  {
+    "revision": "68e09645afff03b820eb",
+    "url": "/prestamosApp/static/js/2.ede15c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prestamosApp/static/js/2.ede15c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c04e14aa7b61962026",
+    "url": "/prestamosApp/static/js/main.74c49d58.chunk.js"
+  },
+  {
+    "revision": "87943fb3c0cb25ee491a",
+    "url": "/prestamosApp/static/js/runtime-main.38a26128.js"
+  }
+]);
